f92154f8215d3e25dc98397ced4a33005935dad3
virusfatal <henroqueoliveirarocha1@gmail.com>
2025-04-04 23:23:01 -0300

    Atualiza

---
+++ config.js
@@ -0,0 +1,22 @@
+
+// token de seu Cluster do mongoDb
+
+const token = 'mongodb+srv://henriqueoliveirarocha1:N1WXFWlWRHQWXUNF@apibase.b9v79h9.mongodb.net/?retryWrites=true&w=majority&appName=Apibase'; 
+
+module.exports = {
+token
+}
+
+/* @CLOVERMYT */
+
+// Canal: https://youtube.com/@clovermyt
+
+// Canal WhatsApp: https://whatsapp.com/channel/0029Va974hY2975B61INGX3Q
+
+// Instagram: https://www.instagram.com/clovermods?igsh=MmcyMHlrYnhoN2Zk
+
+// Telegram: t.me/cinco_folhas
+
+// Comunidade WhatsApp: https://chat.whatsapp.com/Kc5HLGCIokb37mA36NJrM6
+
+// SE FOR REPOSTAR ME MARCA 🧙‍♂️🍀
